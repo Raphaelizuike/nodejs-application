ccd8436b57f794a0f09d24fec9188c31484aa726
Ismaila Afuwape <55412994+afuwapemoyo@users.noreply.github.com>
2021-12-19 23:54:40 -0500

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,12 +1,14 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
-sonarqubeScanner({
-  serverUrl: 'http://3.236.129.45:9000/',
+sonarqubeScanner({	
+  serverUrl: 'http://100.24.24.206:9000/',
+//   serverUrl: 'http://3.236.129.45:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsLandmarkTechnologies',
-	    'sonar.login': '3e9ee4f64a40359b3f9c904bc34392412e2c9d80',
+	    'sonar.login': 'b061f9352863cc8d7334d75ff3c8b078e031556b',
+//          'sonar.login': '3e9ee4f64a40359b3f9c904bc34392412e2c9d80',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
